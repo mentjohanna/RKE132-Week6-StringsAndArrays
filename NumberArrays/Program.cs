ff1d63e804aad20295f418c23a259c170bfdbe10
Johanna Kruusement <kruusement.johanna@gmail.com>
2024-12-27 15:07:24 +0200

    add week6 code

---
+++ NumberArrays/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] numbers = new int[10];
+Random random = new Random();
+
+int sum = 0;
+
+//populating the array
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = random.Next(1, numbers.Length + 1);
+    sum += numbers[i];
+}
+foreach (int number in numbers)
+{
+    Console.WriteLine(number);
+}
+Console.WriteLine($"Total: {sum}");
---
+++ RandomElement/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] snacks = { "sushi", "burger", "pizza", "chicken wings", "Asian takeway" };
+
+Random rand = new Random();
+
+int randomIndex = rand.Next(0, snacks.Length);
+
+Console.WriteLine($"Tonight we are going to eat {snacks[randomIndex]}.");
